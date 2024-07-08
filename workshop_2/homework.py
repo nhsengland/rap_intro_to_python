a1fe715dc3d69e83dfcaa73aef4985b31677b18e
jenniferstruthers1-nhs <jennifer.struthers1@nhs.net>
2024-07-08 11:59:59 +0000

    added conditional homework

--- workshop_2/homework.py
+++ workshop_2/homework.py
@@ -1,1 +1,84 @@
 # Programming is best learnt through doing!
+# This is some homework to complete before the next session.
+
+
+# Conditionals
+# Uncomment the code and replace the ???? with the correct piece of code to obtain the expected output
+"""
+# Expected Output: Good morning
+time_of_day = "morning"
+if ??????:
+    print("Good morning!")
+"""
+
+
+"""
+# Expected Output: Relax at home
+day = "Saturday"
+if day == "Monday":
+    print("Go to work")
+elif ??????:
+    print("Relax at home")
+"""
+
+
+"""
+# Expected Output: Have a sandwich
+meal = "lunch"
+if meal == "breakfast":
+    print("Eat some cereal")
+elif ?????:
+    print("Have a sandwich")
+else:
+    print("Enjoy a nice dinner")
+"""
+
+
+"""
+# Expected Output: You can enter the concert
+age = 20
+has_ticket = True
+if (????? >= 18) and ?????:
+    print("You can enter the concert")
+elif (???? >= 18) and ?????:
+    print("Please buy a ticket!")
+??????:
+    print("You can't enter!")
+"""
+
+"""
+# Expected Output: money_in_bank = 100, has_paid_rent = True
+money_in_bank = 1000
+has_paid_rent = False
+rent_amount = 900
+# We want to check if we have paid rent already, and if we have enough money to pay it
+if ?????(has_paid_rent) and ??????? >= ??????:
+    money_in_bank = money_in_bank - rent_amount
+    has_paid_rent = True
+
+print(money_in_bank)
+print(has_paid_rent)
+"""
+
+
+"""
+# Fix the mistake:
+a = 1
+b = 2
+if a + b == 3
+    print("I love maths!")
+"""
+
+
+"""
+# Fix the mistakes
+name == Rachel
+if name == Kyle
+print I love baseball!
+else name == Lilidh
+print I love hockey
+else
+print I love sports!
+"""
+
+# Loops
