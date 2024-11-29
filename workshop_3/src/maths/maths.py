61cff292d3a70483216169412c4c2c16a8a7c6cc
OwenNHS <owen.kennedy1@nhs.net>
2024-11-29 10:37:48 +0000

    finished homework for training 3 - adding all files

--- workshop_3/src/maths/maths.py
+++ workshop_3/src/maths/maths.py
@@ -18,3 +18,22 @@ def add_two_numbers(first_number, second_number):
     sum_of_numbers = first_number + second_number
 
     return sum_of_numbers
+
+def square_numbers(input_value):
+    """
+    Returns the square of a numerical input
+
+    Parameters
+    ----------
+    input_value (int) : numerical input
+
+    Returns
+    -------
+    
+    int : the square of the input value
+    
+    """
+
+    num_squared = input_value**2
+    
+    return num_squared
