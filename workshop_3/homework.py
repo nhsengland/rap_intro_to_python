b4657ff4295e314e91c963716ce224d62909500d
Rosalind Batty <rosalind.batty@nhs.net>
2024-12-03 15:13:26 +0000

    finished homework

--- workshop_3/homework.py
+++ workshop_3/homework.py
@@ -11,14 +11,14 @@
 
 
 # 2) Now that you have created your function, you need to import the file that it's in. Do that below:
-from ???? import ????
+from src.maths import maths
 
 
 # 3) Now define a list of numbers. They can be any numbers:
-list_of_numbers = ????
+list_of_numbers = [1,2,3,4,5,6,7,8,9,10]
 
 
 # 4) Finally, update the code below so that you loop through list_of_numbers, get the square of each one using your function, and then print it:
-for ????
-    ????
-    print(????)
+for i in list_of_numbers:
+    answer = maths.square_a_number(i)
+    print(answer)
