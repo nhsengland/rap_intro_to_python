d2a5f8853cb2cb170bb526db36864ee47bf9f897
Rosalind Batty <rosalind.batty@nhs.net>
2024-12-03 15:14:18 +0000

    edited maths file for homework

--- workshop_3/src/maths/maths.py
+++ workshop_3/src/maths/maths.py
@@ -18,3 +18,23 @@ def add_two_numbers(first_number, second_number):
     sum_of_numbers = first_number + second_number
 
     return sum_of_numbers
+
+def square_a_number(number):
+    """
+    squares a number and returns the result.
+
+    Parameters
+    ----------
+
+    number (int) : the first number
+   
+    Returns
+    -------
+
+    int : the square of the number
+
+    """
+    square_of_number = number*number
+
+    return square_of_number
+    
